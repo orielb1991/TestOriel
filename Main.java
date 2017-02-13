fa6c64eed5cc48e92e212c416a390f38637b6e8d
Oriel Badner <orielb@orielb-mac.jfrog.internal>
2017-02-13 16:33:41 +0200

    second commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please insert a number");
+        int MyNumber = scanner.nextInt();
+        System.out.println("Your number is :"+ MyNumber);
+        }
+}
